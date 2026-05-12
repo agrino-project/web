358c99201622fd9a886a67746864fca0da394222
Abolfazl Mohammadizadeh <abolfazl.mohammadizadeh@gmail.com>
2026-05-12 10:42:50 +0330

    feat: redesign login page with new styles

--- src/components/views/rooms/RoomHeader/RoomHeader.tsx
+++ src/components/views/rooms/RoomHeader/RoomHeader.tsx
@@ -259,7 +259,7 @@ function RoomHeaderButtons({
             ) : (
                 <>
                     {/* {!isVideoRoom && videoCallButton} */}
-                    {!useElementCallExclusively && !isVideoRoom && voiceCallButton}
+                    {false && !useElementCallExclusively && !isVideoRoom && voiceCallButton}
                 </>
             )}
 
