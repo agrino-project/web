a9d88b616454baf468704548718747751c015198
Abolfazl Mohammadizadeh <abolfazl.mohammadizadeh@gmail.com>
2026-05-21 12:29:33 +0330

    feat: add new icons and fix colors and gredients in home page

--- src/components/views/agriculture/AgriculturePage.tsx
+++ src/components/views/agriculture/AgriculturePage.tsx
@@ -7,15 +7,24 @@ Please see LICENSE files in the repository root for full details.
 
 import React, { useContext, useCallback, useRef, useState, useEffect } from "react";
 import { Icon as ConsultantIcon } from "../../../../res/img/element-icons/agriculture-consultant2.svg";
-import { Icon as OrganizationIcon } from "../../../../res/img/element-icons/agriculture-organization.svg";
-import { Icon as MarketIcon } from "../../../../res/img/element-icons/agriculture-market.svg";
-import { Icon as EducationIcon } from "../../../../res/img/element-icons/agriculture-education.svg";
-import { Icon as ClimateIcon } from "../../../../res/img/element-icons/agriculture-climate.svg";
-import { Icon as ClubIcon } from "../../../../res/img/element-icons/agriculture-club.svg";
-import { Icon as InsuranceIcon } from "../../../../res/img/element-icons/agriculture-insurance.svg";
+// import { Icon as OrganizationIcon } from "../../../../res/img/element-icons/agriculture-organization.svg";
+// import { Icon as MarketIcon } from "../../../../res/img/element-icons/agriculture-market.svg";
+import { Icon as EducationIcon } from "../../../../res/img/element-icons/agriculture-education2.svg";
+// import { Icon as ClimateIcon } from "../../../../res/img/element-icons/agriculture-climate.svg";
+import { Icon as TallarIcon } from "../../../../res/img/element-icons/messageTallar.svg";
+import { Icon as BankIcon } from "../../../../res/img/element-icons/BankIcon.svg";
+import { Icon as BankGredientIcon } from "../../../../res/img/element-icons/BankGredient.svg";
+import { Icon as BuildingsIcon } from "../../../../res/img/element-icons/Buildings.svg";
+// import { Icon as ClubIcon } from "../../../../res/img/element-icons/agriculture-club2.svg";
+import { Icon as InsuranceIcon } from "../../../../res/img/element-icons/agriculture-insurance2.svg";
 import { Icon as BazaarIcon } from "../../../../res/img/element-icons/agriculture-bazaar2.svg";
-import bazaarBackground from "../../../../res/img/element-icons/bazaarBackground.svg";
-import secondBackground from "../../../../res/img/element-icons/secondBackground.svg";
+import { Icon as BazaarGrediantIcon } from "../../../../res/img/element-icons/agriculture-bazaar3.svg";
+import { Icon as BazaargahIcon } from "../../../../res/img/element-icons/bazaargah.svg";
+import { Icon as WeatherIcon } from "../../../../res/img/element-icons/weather.svg";
+import { Icon as ClubIcon } from "../../../../res/img/element-icons/club.svg";
+import { Icon as FarmerHand } from "../../../../res/img/element-icons/farmerHand.svg";
+import ATMBackground from "../../../../res/img/element-icons/ATM.png";
+import GroceryBackground from "../../../../res/img/element-icons/groceryShop.png";
 
 import "../../../../res/css/views/agriculture/AgriculturePage.pcss";
 import MatrixClientContext from "../../../contexts/MatrixClientContext";
@@ -48,13 +57,9 @@ const AgricultureCard: React.FC<AgricultureCardProps> = ({
             onClick={disabled ? undefined : onClick}
             role={disabled ? undefined : "button"}
             tabIndex={disabled ? undefined : 0}
-            style={
-                background
-                    ? {
-                          background: `url(${background}) center/cover no-repeat`,
-                      }
-                    : undefined
-            }
+            style={{
+                background: background ? `url(${background}) center/cover no-repeat` : undefined,
+            }}
         >
             <div className="mx_AgriculturePage_card_icon">
                 <Icon className="mx_AgriculturePage_card_icon_svg" />
@@ -81,7 +86,9 @@ const BannerSlide: React.FC<BannerSlideProps> = ({ title, subtitle, onClick }) =
                 <h3>{title}</h3>
                 <p>{subtitle}</p>
             </div>
-            <div className="mx_AgriculturePage_banner_image">{/* Image placeholder */}</div>
+            <div className="mx_AgriculturePage_banner_image">
+                <FarmerHand />
+            </div>
         </div>
     );
 };
@@ -165,7 +172,7 @@ const AgriculturePage: React.FC = () => {
     const bannerSlides = [
         {
             title: "بهترین قیمت نهاده‌ها در آگرینو",
-            subtitle: "کلیک کنید",
+            subtitle: "کلیک کنید !",
         },
         {
             title: "محصولات ارگانیک با تخفیف ویژه",
@@ -196,16 +203,16 @@ const AgriculturePage: React.FC = () => {
                 {/* Top Row - Two Cards */}
                 <div className="mx_AgriculturePage_row mx_AgriculturePage_row_top">
                     <AgricultureCard
-                        title={_t("custom_panels|agriculture_bazaar")}
-                        icon={BazaarIcon}
-                        background={bazaarBackground}
+                        title={_t("custom_panels|banking_services")}
+                        icon={BankGredientIcon}
+                        background={ATMBackground}
                         disabled={true}
                         variant="default"
                     />
                     <AgricultureCard
                         title={_t("custom_panels|agriculture_market")}
-                        icon={BazaarIcon}
-                        background={secondBackground}
+                        icon={BazaarGrediantIcon}
+                        background={GroceryBackground}
                         onClick={() => handleMarketClick("@advertisebot:agridemo.ir")}
                         variant="default"
                     />
@@ -240,7 +247,7 @@ const AgriculturePage: React.FC = () => {
                     />
                     <AgricultureCard
                         title={_t("custom_panels|agriculture_climate")}
-                        icon={ClimateIcon}
+                        icon={WeatherIcon}
                         disabled={true}
                         variant="small"
                     />
@@ -275,14 +282,14 @@ const AgriculturePage: React.FC = () => {
                     <AgricultureCard
                         title={_t("custom_panels|interaction_with_organizations")}
                         subtitle={_t("custom_panels|for_iranian_agricultural_communities")}
-                        icon={OrganizationIcon}
+                        icon={BuildingsIcon}
                         disabled={true}
                         variant="large"
                     />
                     <AgricultureCard
                         title={_t("custom_panels|discussion_forum")}
                         subtitle={_t("custom_panels|local_agriculture_for_each_other")}
-                        icon={ConsultantIcon}
+                        icon={TallarIcon}
                         disabled={true}
                         variant="large"
                     />
@@ -304,7 +311,7 @@ const AgriculturePage: React.FC = () => {
                     />
                     <AgricultureCard
                         title={_t("custom_panels|banking_services")}
-                        icon={MarketIcon}
+                        icon={BankIcon}
                         disabled={true}
                         variant="small"
                     />
@@ -341,52 +348,21 @@ const AgriculturePage: React.FC = () => {
                     <div className="mx_AgriculturePage_support_cards">
                         <div className="mx_AgriculturePage_support_card">
                             <div className="mx_AgriculturePage_support_icon">
-                                <svg width="40" height="40" viewBox="0 0 24 24" fill="none">
-                                    <path
-                                        d="M20 7C20 5.897 19.103 5 18 5H6C4.897 5 4 5.897 4 7V17C4 18.103 4.897 19 6 19H18C19.103 19 20 18.103 20 17V7Z"
-                                        stroke="currentColor"
-                                        strokeWidth="2"
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                    />
-                                    <path
-                                        d="M4 7L12 13L20 7"
-                                        stroke="currentColor"
-                                        strokeWidth="2"
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                    />
-                                </svg>
+                                <BazaargahIcon />
                             </div>
                             <h4 className="mx_AgriculturePage_support_card_title">{_t("custom_panels|Marketplace")}</h4>
                         </div>
 
                         <div className="mx_AgriculturePage_support_card">
                             <div className="mx_AgriculturePage_support_icon">
-                                <svg width="40" height="40" viewBox="0 0 24 24" fill="none">
-                                    <circle cx="12" cy="12" r="3" stroke="currentColor" strokeWidth="2" />
-                                    <circle cx="12" cy="12" r="8" stroke="currentColor" strokeWidth="2" />
-                                </svg>
+                                <ClubIcon />
                             </div>
                             <h4 className="mx_AgriculturePage_support_card_title">{_t("custom_panels|Club")}</h4>
                         </div>
 
                         <div className="mx_AgriculturePage_support_card">
                             <div className="mx_AgriculturePage_support_icon">
-                                <svg width="40" height="40" viewBox="0 0 24 24" fill="none">
-                                    <path
-                                        d="M3 16.5C3 19.5 5.5 21 8 21C10.5 21 12 19.5 12 16.5V7.5C12 4.5 14.5 3 17 3C19.5 3 21 4.5 21 7.5"
-                                        stroke="currentColor"
-                                        strokeWidth="2"
-                                        strokeLinecap="round"
-                                    />
-                                    <path
-                                        d="M3 7.5L3 16.5"
-                                        stroke="currentColor"
-                                        strokeWidth="2"
-                                        strokeLinecap="round"
-                                    />
-                                </svg>
+                                <WeatherIcon />
                             </div>
                             <h4 className="mx_AgriculturePage_support_card_title">{_t("custom_panels|Weather")}</h4>
                         </div>
